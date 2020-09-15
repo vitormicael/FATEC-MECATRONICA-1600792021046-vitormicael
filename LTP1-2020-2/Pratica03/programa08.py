e546f0293083dffc6533d10b03e1183dbcf76aca
vitormicael <70946454+vitormicael@users.noreply.github.com>
2020-09-15 08:12:23 -0300

    Update programa08.py

--- LTP1-2020-2/Pratica03/programa08.py
+++ LTP1-2020-2/Pratica03/programa08.py
@@ -1,1 +1,5 @@
-
+idade = int(input('Informe a sua idade: '))
+if idade >= 18: 
+  print('Você possui 18 anos ou mais')
+  print('Agora" Você pode ver todas as páginas de jogos da Steam')
+print('Final do programa')

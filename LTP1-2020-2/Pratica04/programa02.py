1d2e0cf01a7a21b044cccc5a32ad13434926ab18
vitormicael <70946454+vitormicael@users.noreply.github.com>
2020-09-15 08:34:18 -0300

    Update programa02.py

--- LTP1-2020-2/Pratica04/programa02.py
+++ LTP1-2020-2/Pratica04/programa02.py
@@ -1,6 +1,6 @@
-senha_secreta=123456
+senha_secreta='15092020'
 senha=input('Informe a sua senha: ')
-if senha == senha_secreta:
+if senha==senha_secreta:
  print('Acesso permitido')
-if senha != senha_secreta:
+if senha!=senha_secreta:
  print('Acesso negado')

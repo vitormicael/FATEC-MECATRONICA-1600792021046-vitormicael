99da7c4e12b33191d10454ac211e0a0fc3ef123e
vitormicael <70946454+vitormicael@users.noreply.github.com>
2020-09-29 07:57:51 -0300

    Create exercício04.py

---
+++ LTP1-2020-2/Pratica05/exercício04.py
@@ -0,0 +1,26 @@
+operacao = input('Informe a operacao desejada:')
+if operacao == '+':
+  valor1 = float(input('Valor 1:'))
+  valor2 = float(input('Valor 2:'))
+  resultado = valor1 + valor2
+  print(resultado)
+elif operacao == '-':
+  valor1 = float(input('Valor 1:'))
+  valor2 = float(input('Valor 2:'))
+  resultado = valor1 - valor2
+  print(resultado)
+elif operacao == '*':
+  valor1 = float(input('Valor 1:'))
+  valor2 = float(input('Valor 2:'))
+  resultado = valor1 * valor2
+  print(resultado)
+elif operacao == '/':
+  valor1 = float(input('Valor 1:'))
+  valor2 = float(input('Valor 2:'))
+  if valor2 == 0:
+    print('Tentativa de divisão por zero!')
+  else:
+    resultado = valor1 / valor2
+    print(resultado)
+else:
+  print('Operacao inválida!')

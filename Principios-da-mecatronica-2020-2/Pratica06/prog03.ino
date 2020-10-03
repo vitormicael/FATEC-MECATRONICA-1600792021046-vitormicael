4097b9808a81737f479cdee7cda6375e88ad0392
vitormicael <70946454+vitormicael@users.noreply.github.com>
2020-10-03 20:09:36 -0300

    Create prog03.ino

---
+++ Principios-da-mecatronica-2020-2/Pratica06/prog03.ino
@@ -0,0 +1,22 @@
+const int LED_VERDE=10;
+const int LED_AMARELO=11;
+const int LED_VERMELHO=12;
+const int BOTAO_1=9;
+const int BOTAO_2=8;
+
+void setup()
+{
+  pinMode(LED_VERMELHO, OUTPUT);
+  pinMode(LED_AMARELO, OUTPUT);
+  pinMode(LED_VERDE, OUTPUT);
+  pinMode(BOTAO_1, INPUT);
+  pinMode(BOTAO_2, INPUT);
+}
+
+void loop()
+{
+  if (digitalRead(BOTAO_1) == HIGH)
+    digitalWrite(LED_VERDE, HIGH);
+   else 
+    digitalWrite(LED_VERDE, LOW);
+}

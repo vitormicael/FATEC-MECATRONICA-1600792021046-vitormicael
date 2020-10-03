714198531c262decc6f055e16fc3843f1a29c95f
vitormicael <70946454+vitormicael@users.noreply.github.com>
2020-10-03 19:11:04 -0300

    Update prog2-Professor.ino

--- Principios-da-mecatronica-2020-2/Pratica06/prog2-Professor.ino
+++ Principios-da-mecatronica-2020-2/Pratica06/prog2-Professor.ino
@@ -1,17 +1,20 @@
 void setup()
 {
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(8, INPUT);
-  pinMode(9, INPUT);
+//Configurar os pinos de saída
+pinMode(12, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(10, OUTPUT);
+//Configurar os pinos de entrada
+pinMode(9, INPUT);
+pinMode(8, INPUT);
 }
 
 void loop()
 {
-  if (digitalRead(8)==HIGH) {
-    digitalWrite(10, HIGH);
-  } else {
-    digitalWrite(10, LOW);
-  }
+//Aciona a saída 10 quando a entrada 8 for acionada
+if (digitalRead(8) == HIGH){
+digitalWrite(10, HIGH);
+} else {
+digitalWrite(10, LOW);
+}
 }

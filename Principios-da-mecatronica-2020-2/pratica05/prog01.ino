cdab858b9a1c401cd3df13a3c21956a78ee31684
vitormicael <70946454+vitormicael@users.noreply.github.com>
2020-10-03 17:30:09 -0300

    Create prog01.ino

---
+++ Principios-da-mecatronica-2020-2/pratica05/prog01.ino
@@ -0,0 +1,18 @@
+void setup()
+{
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(12, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+  delay(500);
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  delay(500);
+}

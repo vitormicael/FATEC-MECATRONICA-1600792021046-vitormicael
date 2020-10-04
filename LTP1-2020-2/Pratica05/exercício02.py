95555c91f42edacd8c03ab66baeb07d0d25d7529
vitormicael <70946454+vitormicael@users.noreply.github.com>
2020-10-04 20:22:55 -0300

    Update exercício02.py

--- LTP1-2020-2/Pratica05/exercício02.py
+++ LTP1-2020-2/Pratica05/exercício02.py
@@ -1,8 +1,11 @@
+#informe o valor dos lados
 lado1 = int(input('Lado 1:'))
 lado2 = int(input('Lado 2:'))
 lado3 = int(input('Lado 3:'))
+#primeiro método
 if (lado1 > 0) and (lado2 > 0) and (lado3 > 0):
   if (lado1+lado2) > lado3 and (lado2+lado3) > lado1 and (lado1 + lado3) > lado2:
     print('Pode formar um trinagulo!')
+#segundo método
 if (lado1 > 0) and (lado2 > 0) and (lado3 > 0) and (lado1+lado2) > lado3 and (lado2+lado3) > lado1 and (lado1 + lado3) > lado2:
   print('Pode formar um trinagulo!')

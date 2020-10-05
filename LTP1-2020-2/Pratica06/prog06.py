98b7ceeae3245db5e4910229920718cb2fcfc357
vitormicael <70946454+vitormicael@users.noreply.github.com>
2020-10-05 17:49:47 -0300

    Create prog06.py

---
+++ LTP1-2020-2/Pratica06/prog06.py
@@ -0,0 +1,22 @@
+## classificador de nível gamer
+#mouse com RGB
+#teclado com RGB
+#monitor com RGB
+#mouse - teclado - monitor - classificação
+#SIM   -  SIM    -   SIM   - Viciado
+#NÃO   -  SIM    -   NÃO   - NOIA
+#SIM   -  NÃO    -   NÃO   - QUASE NORMAL
+#NÃO   -  NÃO    -   NÃO   - -50fps
+print('Descubra seu nível Gamer!!')
+mouse = input('Seu mouse tem RGB?')
+teclado = input('Seu teclado tem RGB?')
+monitor = input('Seu monitor tem RGB?')
+
+if mouse == 'Sim' and teclado == 'Sim' and monitor == 'Sim':
+  print('Viciado!!')
+elif mouse == 'Não' and teclado == 'Sim' and monitor == 'Não':
+  print('Noia')
+elif mouse == 'Sim' and teclado == 'Não' and monitor == 'Não':
+  print('Quase normal')
+else:
+  print('-50fps')

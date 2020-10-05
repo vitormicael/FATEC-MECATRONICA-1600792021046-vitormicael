055d3393bd1e4cc442b1c361f0fa4e9254e250de
vitormicael <70946454+vitormicael@users.noreply.github.com>
2020-10-05 16:46:42 -0300

    Create prog03.py

---
+++ LTP1-2020-2/Pratica06/prog03.py
@@ -0,0 +1,6 @@
+#Ler dois números informados pelo usuario e somar ambos
+print('A soma de dois números')
+numero_1 = float(input('Digite o primeiro número: '))
+numero_2 = float(input('Digite o segundo número: '))
+resultado = (numero_1 + numero_2)
+print('A soma de ambos os números é', resultado)

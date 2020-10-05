591154b38082dc2ea7009f392365a54228c4e516
vitormicael <70946454+vitormicael@users.noreply.github.com>
2020-10-05 18:45:14 -0300

    Create prog08.py

---
+++ LTP1-2020-2/Pratica06/prog08.py
@@ -0,0 +1,11 @@
+soma = 0
+contador = 0
+
+while contador < 3:
+  numero = int(input('Número: '))
+  soma = soma + numero
+  contador = contador + 1 
+
+media = soma / 3
+
+print('O Valor médio é', media)

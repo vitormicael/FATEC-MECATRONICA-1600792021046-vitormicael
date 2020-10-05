f73eaa3524c3688544ce7ab474cf05fb6e213fab
vitormicael <70946454+vitormicael@users.noreply.github.com>
2020-10-05 17:14:53 -0300

    Update prog04.py

--- LTP1-2020-2/Pratica06/prog04.py
+++ LTP1-2020-2/Pratica06/prog04.py
@@ -2,7 +2,7 @@
 temp_limite = float(input('Informe uma temperatura limite: '))
 temp_atual = float(input('Informe a temperatura atual: '))
 ##
-if temp_atual >= temp_limite:
+if temp_atual > temp_limite:
   print('A temperatura está acima do limite adequado')
-elif temp_atual <= temp_limite:
+if temp_atual < temp_limite:
   print('A temperatura está abaixo do limite')

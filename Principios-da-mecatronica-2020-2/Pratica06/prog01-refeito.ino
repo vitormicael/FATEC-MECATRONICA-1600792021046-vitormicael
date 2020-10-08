cda474c48f66948c53fab0ddde379c051b39cad7
vitormicael <70946454+vitormicael@users.noreply.github.com>
2020-10-08 06:11:56 -0300

    Create prog01-refeito.ino

---
+++ Principios-da-mecatronica-2020-2/Pratica06/prog01-refeito.ino
@@ -0,0 +1,17 @@
+void setup()
+{
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, INPUT);
+  pinMode(8, INPUT);
+}
+
+void loop()
+{
+  if (digitalRead(8) == HIGH) {
+    digitalWrite(10, HIGH);
+  } else {
+    digitalWrite(10, LOW);
+  }
+}

31fe2358bc094b3867e19e7eaadedec20cbb941a
vitormicael <70946454+vitormicael@users.noreply.github.com>
2020-10-31 16:44:25 -0300

    Create projeto-parte5.py

---
+++ LTP1-2020-2/Projeto/projeto-parte5.py
@@ -0,0 +1,30 @@
+
+import random
+repeticao = 0
+senha_id1 = '2468'
+senha_id2 = '123456'
+senha_id3 = '1234'
+saldo_ID1 = 1000
+saldo_ID2 = 250
+saldo_ID3 = 3000
+resposta = 'Sim'
+Sim = True
+usuario = input('Informe o seu ID: ')
+senha = input('Informe a sua senha: ')
+email = input('Informe o seu e-mail: ')
+while resposta == 'Sim':
+ if usuario == 'id1':
+  if senha == senha_id1:
+    recebedor = input('Informe o ID recebedor do pagamento: ')
+    print('Seu saldo atual é de R$',saldo_ID1)
+    valor_pagamento = float(input('Informe o valor a ser pago: '))
+    if saldo_ID1 > valor_pagamento:
+      pagamento = (saldo_ID1-valor_pagamento)
+      saldo_atual = (saldo_ID1+valor_pagamento)
+      print('Transação realizada com sucesso')
+      print('Transação realizada por:', usuario)
+      print('Transação realizada para:', recebedor)
+      print('Valor da transação R$',valor_pagamento)
+      print("sua chave de segurança é:"),print(random.randrange(1000, 9999))
+      print(email)
+      resposta = input('Deseja realizar outra transação? (Sim ou Não) ')
